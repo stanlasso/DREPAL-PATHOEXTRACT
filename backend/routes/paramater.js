e02f8a57ab2192e3c7da7b28403edf324e0f2710
abouchou <christianpaulako@gmail.com>
2023-03-29 16:01:34 +0000

    deleted backend

--- backend/routes/paramater.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const parameters = require('../controllers/parameters');
-router.post('/add',parameters.create);
-router.get('/list',parameters.read);
-router.get('/param/:fullname',parameters.getparameter);
-router.patch('/update',parameters.update);
-router.delete('/delete/:fullname',parameters.deleteparameter);
-module.exports = router;
--- backend/routes/reference.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const references = require('../controllers/references');
-router.post('/add',references.create);
-router.get('/list',references.read);
-router.patch('/update/:username',references.update);
-router.delete('/delete/:username',references.deletereference);
-module.exports = router;
